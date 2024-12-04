54aa94546059550dc33b761b3297128b0ea45d27
Marcus Almeida <mpereirassa@gmail.com>
2024-12-04 10:27:04 -0300

    chore(seeds.rb): remove comentários desnecessários e adiciona dados de exemplo feat(seeds.rb): cria disciplina e conteúdos para uso na aplicação

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+#  Create the disciplines and contents that will be used in the application.
+
+discipline = FactoryBot.create(:discipline, title: 'Introdução a linguagem GO')
+FactoryBot.create_list(:content, 5, discipline: discipline, kind: :text)
