ef0962d210f2a254b7f81568abb087b6ca32a142
Marcus Almeida <mpereirassa@gmail.com>
2024-12-21 05:36:01 -0300

    Implementando autenticação
    feat(auth): implementa sistema de autenticação com gerenciamento de senhas
    feat(auth): adiciona suporte para sessões de usuário e recuperação de senha
    feat(auth): cria controladores e visualizações para gerenciamento de senhas
    feat(auth): implementa conexão do ActionCable para autenticação em tempo real
    feat(auth): adiciona migrações para criação de tabelas de usuários e sessões
    chore(routes): define rotas para sessões e recuperação de senhas
    
    feat(schema.rb): atualiza a versão do esquema e adiciona tabelas de sessions e users
    feat(seeds.rb): adiciona usuários de exemplo para o seeding do banco de dados
    feat(users.rb): cria factory para o modelo User com atributos gerados
    test(user_spec.rb): adiciona testes para validações e comportamento do modelo User
    feat(passwords_mailer_preview.rb): adiciona pré-visualização para o mailer de senhas

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 
 discipline = FactoryBot.create(:discipline, title: 'Introdução a linguagem GO')
 FactoryBot.create_list(:content, 5, discipline: discipline, kind: :text)
+FactoryBot.create(:user, email_address: 'student@spacedevs.com.br', password: '123', role: :student)
+FactoryBot.create(:user, email_address: 'manager@spacedevs.com.br', password: '123', role: :manager)
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validates' do
+    it { is_expected.to validate_presence_of(:email_address) }
+    it { is_expected.to validate_presence_of(:registration_code) }
+    it { is_expected.to have_many(:sessions) }
+    it { is_expected.to define_enum_for(:role).with_values(manager: 0, student: 1) }
+  end
+
+  context 'auto generate registration code' do
+    let(:user) { create(:user) }
+
+    it 'should generate when the user has been registrated' do
+      expect(user.registration_code).to be_present
+    end
+  end
+end
