9fc4612180e2449d598a5b9f25a8910e615baec2
Marcus Almeida <mpereirassa@gmail.com>
2024-12-21 05:36:01 -0300

    Implementando autenticação
    feat(auth): implementa sistema de autenticação com gerenciamento de senhas
    feat(auth): adiciona suporte para sessões de usuário e recuperação de senha
    feat(auth): cria controladores e visualizações para gerenciamento de senhas
    feat(auth): implementa conexão do ActionCable para autenticação em tempo real
    feat(auth): adiciona migrações para criação de tabelas de usuários e sessões
    chore(routes): define rotas para sessões e recuperação de senhas
    
    feat(schema.rb): atualiza a versão do esquema e adiciona tabelas de sessions e users
    feat(seeds.rb): adiciona usuários de exemplo para o seeding do banco de dados
    feat(users.rb): cria factory para o modelo User com atributos gerados
    test(user_spec.rb): adiciona testes para validações e comportamento do modelo User
    feat(passwords_mailer_preview.rb): adiciona pré-visualização para o mailer de senhas

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :email_address, :registration_code, presence: true
+
+  enum :role, manager: 0, student: 1
+end
