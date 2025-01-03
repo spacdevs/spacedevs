32afccb5d9f7d1876e151f4954944203da7f67fe
Marcus Almeida <mpereirassa@gmail.com>
2025-01-03 11:01:47 -0300

    feat(importmap): add uikit framework

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
+import "uikit"
+import "uikit-icons";
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
