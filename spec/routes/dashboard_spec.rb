7d1fcbf0b92961bcc4c2297ace5fa56c19222c9a
Marcus Almeida <mpereirassa@gmail.com>
2025-01-05 18:44:45 -0300

    feat(dashboard_spec.rb): adiciona testes de roteamento para o dashboard (#33)

---
+++ spec/routes/dashboard_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Sessions", type: :routing do
+  context "GET /" do
+    it { expect(get: "/").to route_to(controller: "dashboard", action: "index") }
+  end
+end
