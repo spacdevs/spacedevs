6cdca0ae6428591d9165723ae1bc07eb26c6a04f
Marcus Almeida <mpereirassa@gmail.com>
2025-01-31 08:31:22 -0300

    test(user_spec.rb): organiza os testes em contextos e adiciona validações de associação feat(user_spec.rb): adiciona teste para a associação belong_to(:school)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe User, type: :model do
   context 'validates' do
     it { is_expected.to validate_presence_of(:email_address) }
     it { is_expected.to validate_presence_of(:registration_code) }
-    it { is_expected.to have_many(:sessions) }
     it { is_expected.to define_enum_for(:role).with_values(manager: 0, student: 1) }
+  end
+
+  context 'association' do
+    it { is_expected.to have_many(:sessions) }
     it { is_expected.to have_one(:profile) }
+    it { is_expected.to belong_to(:school) }
   end
 
   context 'auto generate registration code' do
