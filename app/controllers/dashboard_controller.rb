f8ecc8163c75fb39448bb52a7554b3b8a7669393
Marcus Almeida <mpereirassa@gmail.com>
2025-01-31 07:36:14 -0300

    fix(dashboard_controller.rb): adiciona verificação se o usuário é estudante para notificação de perfil vazio

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,8 +15,13 @@ class DashboardController < ApplicationController
     @notifications = []
     support_url = ENV.fetch("COMMUNITY_URL", "https://www.spacedevs.com.br")
 
-    @notifications << "Termine de configurar seu perfil para ter acesso ao conteúdo" if current_user.profile.blank?
-    @notifications << "Estamos trabalhando no conteúdo para te ofertar, " \
-                      "enquanto isso acesse a nossa <a href='#{support_url}'>comunidade</a>." if @disciplines.blank?
+    if current_user.profile.blank? && current_user.student?
+      @notifications << "Termine de configurar seu perfil para ter acesso ao conteúdo"
+    end
+
+    if @disciplines.blank?
+      @notifications << "Estamos trabalhando no conteúdo para te ofertar, " \
+      "enquanto isso acesse a nossa <a href='#{support_url}'>comunidade</a>."
+    end
   end
 end
