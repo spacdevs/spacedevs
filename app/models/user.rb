b030e982e5e4516148110b2f0f8c0f3e1635c4f4
Marcus Almeida <mpereirassa@gmail.com>
2025-02-07 07:56:49 -0300

    refactor(user.rb): reorganiza associações e normalizações no modelo User test(user_spec.rb): adiciona testes para normalização de email e código de registro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 
 class User < ApplicationRecord
   has_secure_password
-  has_many :sessions, dependent: :destroy
-  has_one  :profile, dependent: :destroy
+
   belongs_to :school
+  has_one    :profile, dependent: :destroy
+  has_many   :sessions, dependent: :destroy
 
   alias_attribute :email, :email_address
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
-
-  validates :email_address, :registration_code, presence: true
+  normalizes :registration_code, with: ->(code) { code.strip.upcase }
 
   enum :role, manager: 0, student: 1
+
+  validates :email_address, :registration_code, presence: true
 end
