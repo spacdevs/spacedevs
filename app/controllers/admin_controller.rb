4148e594c4f2692d11c451b709519902a6abafd5
Marcus Almeida <mpereirassa@gmail.com>
2025-02-15 23:10:15 -0300

    feat(admin_controller): altera a verificação de admin para antes da ação feat(navbar): exibe o menu de administração apenas para usuários admin test(admin/users): adiciona testes para verificar acesso de admin e estudante

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,11 +4,11 @@ class AdminController < ApplicationController
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  helper_method :is_admin?
+  before_action :admin_user?
 
   private
 
-  def is_admin?
-    current_user.admin?
+  def admin_user?
+    redirect_to root_path unless current_user.admin?
   end
 end
--- spec/system/admin/users/index_spec.rb
+++ spec/system/admin/users/index_spec.rb
@@ -4,16 +4,35 @@ feature Admin::UsersController do
   let(:admin) { create(:user, :admin) }
   let!(:student) { create(:user, :with_profile, :student) }
 
-  before do
+  scenario 'check if Administração word exists' do
     login_as(admin)
+
+    expect(page).to have_content('Administração')
   end
 
-  scenario 'view registered users' do
+  scenario 'admin view registered users' do
+    login_as(admin)
     click_on 'Alunos'
 
     expect(current_path).to have_content(admin_users_path)
     expect(page).to have_content(student.profile.fullname)
     expect(page).to have_content(student.email)
     expect(page).to have_content(I18n.l(student.created_at, format: :short))
+  end
+
+  scenario 'student cannot view others registered students' do
+    login_as(student)
+
+    expect(page).not_to have_content('ADMIN')
+    expect(page).not_to have_content('Alunos cadastrados')
+  end
+
+  scenario 'user tries to access a direct route and gets redirected to the home page' do
+    login_as(student)
+    visit admin_users_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).not_to have_content('ADMIN')
+    expect(page).not_to have_content('Alunos cadastrados')
   end
 end
