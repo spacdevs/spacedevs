adbfdfb0fd8120ead6e6657bb079d86a0cf554f5
Marcus Almeida <mpereirassa@gmail.com>
2025-02-22 17:13:32 -0300

    feat(disciplines): adiciona controle e visualização de disciplinas no admin feat(disciplines): limita a exibição de disciplinas a 15 registros fix(disciplines): altera o título da disciplina para usar caracteres aleatórios test(disciplines): cria testes para verificar a visualização e acesso às disciplinas

---
+++ spec/system/admin/disciplines/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature Admin::UsersController do
+  let(:admin) { create(:user, :with_profile, :admin) }
+  let(:student) { create(:user, :with_profile, :student) }
+
+  scenario 'admin sees disciplines already registered' do
+    create(:discipline, title: 'Introdução a computação')
+
+    login_as(admin)
+    click_on 'Disciplinas'
+
+    expect(page).to have_content('Introdução a computação')
+  end
+
+  scenario 'admin sees only 15 disciplines' do
+    create_list(:discipline, 20)
+
+    login_as(admin)
+    visit admin_disciplines_path
+
+    expect(page).to have_css('.discipline', count: 15)
+  end
+
+  scenario 'user tries to access a direct route and gets redirected to the home page' do
+    student = create(:user, :with_profile, :student)
+
+    login_as(student)
+    visit admin_disciplines_path
+
+    expect(current_path).to eq(root_path)
+  end
+end
