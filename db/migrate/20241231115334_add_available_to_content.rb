72d7f1d7bf0b74363bd1994bb6e68521ad1fdbad
Marcus Almeida <mpereirassa@gmail.com>
2025-02-22 18:09:13 -0300

    refactor(migrations): remove migrações obsoletas relacionadas a available_on e slug

--- db/migrate/20241231115334_add_available_to_content.rb
+++
@@ -1,5 +0,0 @@
-class AddAvailableToContent < ActiveRecord::Migration[8.0]
-  def change
-    add_column :contents, :available_on, :datetime
-  end
-end
--- db/migrate/20250109184158_add_slug_to_contents.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToContents < ActiveRecord::Migration[8.0]
-  def change
-    add_column :contents, :slug, :string
-    add_index :contents, :slug, unique: true
-  end
-end
