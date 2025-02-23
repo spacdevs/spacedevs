1a8da5c54e9b60d8c788d621f53e90ad16c331f8
Marcus Almeida <mpereirassa@gmail.com>
2025-02-23 20:20:57 -0300

    refactor(team.rb): remove associações não utilizadas de team_users_discipline test(team_spec.rb): adiciona testes para validação de presença do nome do time

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
-  has_many :team_users_discipline
-  has_many :users, through: :team_users_discipline
-  has_many :disciplines, through: :team_users_discipline
-
   validates :name, presence: true
 end
