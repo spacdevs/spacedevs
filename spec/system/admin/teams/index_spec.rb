176ebca909777df3a7d35d39b06c0e799e083e5e
Marcus Almeida <mpereirassa@gmail.com>
2025-03-11 19:43:09 -0300

    feat(admin): atualiza exibição de turmas para mostrar data de atualização e disponibilidade

--- spec/system/admin/teams/index_spec.rb
+++ spec/system/admin/teams/index_spec.rb
@@ -2,26 +2,33 @@ require 'rails_helper'
 
 feature Admin::TeamsController do
   let(:admin_user) { create(:user, :with_profile, :admin) }
+  let!(:team)      { create(:team, active: true) }
 
-  context 'when found teams' do
-    let!(:teams) { create_list(:team, 3) }
-
-    scenario 'admin views teams' do
-      login_as(admin_user)
-      click_on 'Turmas'
-
-      teams.each do
-        expect(page).to have_content(it.name)
-      end
-    end
+  before do
+    login_as(admin_user)
+    click_on 'Turmas'
   end
 
-  context 'when teams cannot be found' do
-    scenario 'admin cannot views teams' do
-      login_as(admin_user)
-      visit admin_teams_path
+  scenario 'when admin views teams' do
+    expect(page).to have_content(team.name)
+    expect(team.active).to eq(true)
+    expect(page).to have_content('Lançado')
+  end
 
-      expect(page).to have_content('Não há registros de turmas')
-    end
+  scenario 'when team#active is falsey' do
+    team.update(active: false)
+
+    visit admin_teams_path
+
+    expect(team.active).to eq(false)
+    expect(page).to have_content('Indisponível')
+  end
+
+  scenario 'when admin cannot views teams' do
+    Team.destroy_all
+
+    visit admin_teams_path
+
+    expect(page).to have_content('Não há registros de turmas')
   end
 end
