624b6ad9b77bd3dda89a1f6129437033d3161282
Marcus Almeida <mpereirassa@gmail.com>
2025-03-11 15:39:38 -0300

    feat(admin): corrige rota no formulário de criação de turmas e adiciona teste de criação

---
+++ spec/system/admin/teams/create_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature Admin::TeamsController do
+  let(:admin_user) { create(:user, :with_profile, :admin) }
+
+  before do
+    login_as(admin_user)
+    click_on 'Turmas'
+    click_on 'Adicionar'
+  end
+
+  scenario 'creates a team' do
+    fill_in 'Nome', with: 'Colégio Estadual Roberto Santos - Turma 01'
+    find(:css, "#team_active[value='1']").set(true)
+    click_on 'Criar Turma'
+
+    expect(current_path).to eq admin_teams_path
+    expect(Team.count).to eq(1)
+  end
+end
