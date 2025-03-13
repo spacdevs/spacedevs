532b10200cb92a58f0ea1111020f9086f6f30848
Marcus Almeida <mpereirassa@gmail.com>
2025-03-13 18:37:00 -0300

    refactor(tests): remove cenário desnecessário de atualização de conteúdo no controlador de disciplinas

--- spec/system/admin/disciplines/contents/update_spec.rb
+++ spec/system/admin/disciplines/contents/update_spec.rb
@@ -26,12 +26,6 @@ feature Admin::Disciplines::ContentsController do
       expect(content.body).to eq 'Nesta aula você aprenderá os princípios básicos da linguagem Python.'
       expect(content.video?).to be_truthy
     end
-
-    scenario 'admin cannot updated content' do
-      expect(page).to have_content('Introdução a linguagem Python')
-      expect(content.body).to eq 'Nesta aula você aprenderá os princípios básicos da linguagem Python.'
-      expect(content.video?).to be_truthy
-    end
   end
 
   context 'when exists fails in process' do
