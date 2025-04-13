e58cb6a0f07b8eb5a093a617a6f68e47c300d785
Marcus Almeida <mpereirassa@gmail.com>
2025-04-13 10:29:52 -0300

    feat(deploy): Configura ambiente para realizar deployments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 #  Create the disciplines and contents that will be used in the application.
-FactoryBot.create(:discipline, :with_contents)
+# FactoryBot.create(:discipline, :with_contents)
 
 # Create users with different roles
-FactoryBot.create(:user, :with_profile, email_address: 'student@spacedevs.com.br', password: '123', role: :admin, registration_code: 'SDS6658093H')
-FactoryBot.create_list(:user, 5, :with_profile, :student)
+# FactoryBot.create(:user, :with_profile, email_address: 'student@spacedevs.com.br', password: '123', role: :admin, registration_code: 'SDS6658093H')
+# FactoryBot.create_list(:user, 5, :with_profile, :student)
