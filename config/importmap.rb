b0db92be772446f07ecc79a5618a056cfadb2a2c
Marcus Almeida <mpereirassa@gmail.com>
2025-04-14 11:16:09 -0300

    feat: adiciona suporte ao active text

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "uikit", to: "https://cdn.jsdelivr.net/npm/uikit@3.21.16/dist/js/uikit.min.js"
 pin "uikit-icons", to: "https://cdn.jsdelivr.net/npm/uikit@3.21.16/dist/js/uikit-icons.min.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
