50614c621af482b65f93d60295480bdff1068321
Marcus Almeida <mpereirassa@gmail.com>
2025-04-15 12:19:22 -0300

    fix: remove validação para registration_code e configura uma role padrão para o usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,20 @@
 class User < ApplicationRecord
   has_secure_password
 
+  before_validation :generate_registration_code
+
   belongs_to :school
   has_one    :profile, dependent: :destroy
   has_many   :sessions, dependent: :destroy
   has_many   :team_users, dependent: :destroy
   has_many   :teams, through: :team_users
 
-  before_validation :generate_registration_code
-
   alias_attribute :email, :email_address
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   normalizes :registration_code, with: ->(code) { code.strip.upcase }
 
-  enum :role, admin: 0, student: 1
+  enum :role, { admin: 0, student: 1 }, default: :student
 
   validates :registration_code, presence: true
   validates :email_address, presence: true, uniqueness: true, format: {
